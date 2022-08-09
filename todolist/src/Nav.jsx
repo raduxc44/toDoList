04927fd282baa527569410293cc7f4db5619c022
raduxc44 <raduxc44@gmail.com>
2022-08-09 05:21:16 +0300

    Nav implemented

---
+++ todolist/src/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return(
+        <nav>
+            <i class="fa-solid fa-check"></i>
+            <p>TO-DO</p>
+            <i class="fa-solid fa-check"></i>
+        </nav>
+    )
+}
+
+export default Nav
