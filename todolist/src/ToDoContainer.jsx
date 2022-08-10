87be59057f28e3ff0dfb66597e12ddc9e280b342
raduxc44 <raduxc44@gmail.com>
2022-08-10 15:12:59 +0300

    Updated styling on hard-coded todos

--- todolist/src/ToDoContainer.jsx
+++ todolist/src/ToDoContainer.jsx
@@ -1,25 +1,43 @@
-import check from './check-mark.png'
+
 
 function ToDoContainer () {
     return (
         <div className="to-do-container">
-            <div className="todo one">
-                <img src={check} alt="" srcset="" />
-                <p>Brush teeth</p>
-                <p>details</p>
-                <p>remove</p>
+            <div className="todo high-border">
+                <div className="todo-first-half">
+                    <i class="fa-solid fa-check high-check"></i>
+                    <p>Brush teeth</p>
+                </div>
+                <div className="todo-second-half">
+                    <p>details</p>
+                    <p>due-date</p>
+                    <p>edit-butt</p>
+                    <p>remove-butt</p>
+                </div>
             </div>
-            <div className="todo two">
-                <img src={check} alt="" srcset="" />
-                <p>Brush teeth</p>
-                <p>details</p>
-                <p>remove</p>
+            <div className="todo mid-border">
+                <div className="todo-first-half">
+                    <i class="fa-solid fa-check mid-check"></i>
+                    <p>Brush teeth</p>
+                </div>
+                <div className="todo-second-half">
+                    <p>details</p>
+                    <p>due-date</p>
+                    <p>edit-butt</p>
+                    <p>remove-butt</p>
+                </div>
             </div>
-            <div className="todo three">
-                <img src={check} alt="" srcset="" />
-                <p>Brush teeth</p>
-                <p>details</p>
-                <p>remove</p>
+            <div className="todo low-border">
+                <div className="todo-first-half">
+                    <i class="fa-solid fa-check low-check"></i>
+                    <p>Brush teeth</p>
+                </div>
+                <div className="todo-second-half">
+                    <p>details</p>
+                    <p>due-date</p>
+                    <p>edit-butt</p>
+                    <p>remove-butt</p>
+                </div>
             </div>
         </div>
     )
