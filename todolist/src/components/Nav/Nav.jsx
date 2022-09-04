441a13b50113063d0d6a33c31cd2ec27b99ba2fa
raduxc44 <raduxc44@gmail.com>
2022-09-05 00:39:17 +0300

    Moved the content of the Add component into Nav

--- todolist/src/components/Nav/Nav.jsx
+++ todolist/src/components/Nav/Nav.jsx
@@ -1,6 +1,18 @@
 import './Nav.css'
 
 function Nav () {
+
+    function createToDo() {
+        
+        let createForm = document.getElementsByClassName('create-form')[0];
+        createForm.classList.add('visible')
+        let mainPage = document.getElementById('main-page');
+        mainPage.classList.add('blur');
+        mainPage.classList.add('avoid-clicks')
+
+    }
+
+
     return(
         <div className="menu">
             <div className="home">Home</div>
@@ -12,6 +24,7 @@ function Nav () {
                 <div className="interest">Painting</div>
                 <div className="interest">Meditating</div>
             <div className="Notes">Notes</div>
+            <i onClick={createToDo} className="fa-solid add-butt fa-calendar-plus fa-5x"></i>
         </div>
     )
 }
