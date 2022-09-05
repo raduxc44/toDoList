4a9ca8d36231a1ec27206be24e6249c1b06321fa
raduxc44 <raduxc44@gmail.com>
2022-09-05 16:43:18 +0300

    Removed interests and notes

--- todolist/src/components/Nav/Nav.jsx
+++ todolist/src/components/Nav/Nav.jsx
@@ -15,15 +15,10 @@ function Nav () {
 
     return(
         <div className="menu">
-            <div className="home">Home</div>
+            <div className="menu-div-active home">Home</div>
             <div className="today">Today</div>
             <div className="this-week">Week</div>
             <div className="this-month">Month</div>
-            <div className="interests">Interests</div>
-                <div className="interest">Web Dev</div>
-                <div className="interest">Painting</div>
-                <div className="interest">Meditating</div>
-            <div className="Notes">Notes</div>
             <i onClick={createToDo} className="fa-solid add-butt fa-calendar-plus fa-5x"></i>
         </div>
     )
